7df08e4ced5c159e1c8894a3dff9b8dff124265f
Boryamba99 <129422467+Boryamba99@users.noreply.github.com>
2023-10-08 12:40:17 +0400

    Add files via upload
    LeetCode

--- Main.java
+++ Main.java
@@ -1,35 +1,51 @@
-import Model.Apartment;
-import Service.CityService;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
-    public static CityService cityService = new CityService();
-    public static void main(String[] args)
-    {
-        int hours = 0;
-        Apartment[][][] apartments = cityService.generateCity();
-        for (int i = 0; i < 300; i++) {
-            hours = checkTime(hours);
-            System.out.println("Текущее время " + hours);
-            for (int j = 0; j < apartments.length  ; j++) {
-                for (int k = 0; k < apartments[j].length; k++) {
-                    for (int l = 0; l < apartments[j][k].length; l++) {
-                       Apartment apartment = apartments[j][k][l];
-                       cityService.releaseEvent(apartment, hours);
+    public static void main(String[] args) {
+        int[] numbers1 = {2, 11, 7, 1};
+        int target1 = 9;
+        int[] numbers2 = {3, 2, 4};
+        int target2 = 6;
+        int[] numbersEmpty = {3,3};
+        int target3 = 6;
+        int[] negativeNumbers = {-1, -2, -3, -4, -5};
+        int target4 = -8;
 
-                    }
+        int[] result = twoSum(numbers1, target1);
+        int[] result2 = twoSum(numbers2, target2);
+        int[] result3 = twoSum(numbersEmpty, target3);
+        int[] negativeResult = twoSum(negativeNumbers, target4);
+        System.out.println("arr: " + Arrays.toString(result));
+        System.out.println("arr: " + Arrays.toString(result2));
+        System.out.println("arr: " + Arrays.toString(result3));
+        System.out.println("arr: " + Arrays.toString(negativeResult));
+    }
+
+    private static int[] twoSum(int[] args, int target2) {  //int[] numbers1 = {2, , 7, };
+        int[] result = {};
+
+        for (int i = 0; i < args.length - 1; i++) {
+
+            int result1 = target2 - args[i];
+            for (int j = i + 1; j < args.length ; j++) {
+                if ( args[j] == result1) {
+                    result = new int[]{i,j};
                 }
             }
         }
+        return result;
 
     }
 
-    private static int checkTime(int hours)
-    {
-        int currentHours = hours + 1;
-        if(hours > 23){
-            return 0;
-        }
-        return currentHours;
-    }
 }
+
+
+
+
+
+
+
+
+
